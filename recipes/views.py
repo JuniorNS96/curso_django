10299ebcf3e95ecb8fbd0e77563b9e2768b80048
Junior Nunes <josenunes369@gmail.com>
2022-06-04 22:06:29 -0300

    reparting some html codes and import font awesome

--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 # Create your views here.
 
+
 def home(response):
-    return render(response, 'recipes/home.html')
-
-def contato(response):
-    return HttpResponse('Essa é a pagina CONTATO')
-
-
-def sobre(response):
-    return HttpResponse('Essa é a pagina do SOBRE')
+    return render(response, 'recipes/pages/home.html', context={
+        'name': 'Junior'
+    })
