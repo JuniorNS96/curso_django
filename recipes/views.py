18715c3599d88b73c7157a81b2bdea048338f2b4
Junior Nunes <josenunes369@gmail.com>
2022-06-12 18:07:07 -0300

    add css in global for test

--- recipes/views.py
+++ recipes/views.py
@@ -7,3 +7,9 @@ def home(response):
     return render(response, 'recipes/pages/home.html', context={
         'name': 'Junior'
     })
+
+
+def recipes(response, id):
+    return render(response, 'recipes/pages/recipe-view.html', context={
+        'name': 'Junior'
+    })
